0f771aa4b0dd2bfcfe9aaac676a43231a29c792c
pchawla@presidio.com <pchawla@presidio.com>
2024-06-10 16:21:08 -0400

    pipelinetry

--- Terraform/pods/provider.tf
+++ Terraform/pods/provider.tf
@@ -1,3 +1,10 @@
 provider "kubernetes" {
-    config_path = var.kubeconfig_path
+  config_path    = "~/.kube/config"
+  config_context = "my-context"
+}
+
+resource "kubernetes_namespace" "example" {
+  metadata {
+    name = "my-first-namespace"
+  }
 }
